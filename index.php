51f977e1d70c6d35cadd5176f97bfd391cb80a70
TheCopacabanaMan <thecopacabanaman@yahoo.it>
2016-04-29 11:17:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+	/*
+	@botfather:
+	- mandare un comando ( es. /token )
+	- inserire lo username del bot ( es. @Ciccio_TestBot )
+	// */
+	
+	/*
+	Esempio: 
+	https://github.com/D4RKONION/bobbot/blob/master/bobbot.php
+	// */
+
+	// $bottoken = "207919843:AAEeFkhsaHGDykH5T_uRX7O8jLsvag6Nle8";
+	$bottoken = "207919843:AAEeFkhsaHGDykH5T_uRX7O8jLsvag6Nle8";
+	$website = "https://api.telegram.org/bot".$bottoken;
+	
+	// $update = file_get_contents($website. "/getupdates?offset=0");
+	// $update = file_get_contents($website. "/getme");
+	$update = file_get_contents("php://input");
+	if ( strlen($update ) == 0) {
+		echo "Non ho ricevuto niente";
+		return;
+	}
+	
+	$update_array = json_decode($update, TRUE);
+	// print_r( $update_array );
+	
+	$chat_id = $update_array["result"][0]["message"]["chat"]["id"];
+	$text = $update_array["result"][0]["message"]["text"];
+	// print_r( $text. "<br />" );
+	// print_r($chat_id. "<br />"); 
+	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Ciao");
+	
+  
+
+/*
+@BotFather
+
+@Ciccio_TestBot
+
+Riceve le news, le offerte e la ricerca personale di Ernesto Spa.
+
+/setcommands
+@Ciccio_TestBot
+
+start - Mostra un elenco dei comandi
+ultima_news - Mostra l'ultima news
+ricerca_personale - Mostra le richerche di personale
+
+/setprivacy
+@Ciccio_TestBot
+// */
+
+?>
