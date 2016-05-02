d283569fd83c649095566aaee440986c49d153b6
TheCopacabanaMan <thecopacabanaman@yahoo.it>
2016-05-02 10:09:23 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -21,18 +21,23 @@ $text = trim($text);
 $text = strtolower($text);
 
 $response_message = "Ciccio";
+$parse_mode = "Markdown";
 
 if ( $text == "/start" || $text == "/help" ) {
   $response_message = "Questo bot mostra le news, le offerte e la ricerca personale di Ermesto Spa";
   $response_message .= "<br /><br />";
   $response_message .= "Comandi:";
   $response_message .= "<br /><br />";
-  $response_message .= "<b>/start</b> - Mostra l'help";
-  $response_message .= "<br />";
+  $response_message .= "<b>/start</b> - Mostra l'help" . "<br />";
+  $response_message .= "<b>/news</b> - Mostra le news" . "<br />";
+  $response_message .= "<b>/offerte</b> - Mostra le offerte" . "<br />";
+  $response_message .= "<b>/ricerca</b> - Mostra la ricerca personale" . "<br />";
+  
+  $parse_mode = "HTML";
 }  
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $response_message);
+$parameters = array('chat_id' => $chatId, "text" => $response_message, parse_mode => $parse_mode);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
 
